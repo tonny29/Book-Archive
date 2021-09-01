71093a80df924e3642f0acc90ee80d67a1ced4ff
tonny29 <zannatulnayemtonny@gmail.com>
2021-09-02 04:46:31 +0600

    second commit

--- book.js
+++ book.js
@@ -14,6 +14,8 @@ const allBook=()=>{
         .then(res=>res.json())
         .then(data=>displayBook(data.docs));
     }
+    const unknownText=document.getElementById('nuull');
+    unknownText.textContent='';
 }
 // allBook();
 const displayBook=(books)=>{
@@ -21,7 +23,7 @@ const displayBook=(books)=>{
     error.innerHTML='';
     const findBook=document.getElementById('search-books');
     findBook.textContent='';
-    if(books.length==0){
+    if(books.length===0){
         // console.log('type write a right book namwe');
         const nullMassage=document.getElementById('nuull');
         nullMassage.innerHTML=`
@@ -34,19 +36,22 @@ const displayBook=(books)=>{
             const div=document.createElement('div');
             div.classList.add('col');
             div.innerHTML=`
-              <div class="card">
-                
+              <div onclick="details(${book.cover_i})" class="card">
+              <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="..."></img>
                 <div class="card-body">
-                  <h5 class="card-title">${book.title}</h5>
-                  <p class="card-text">${book.author_name}<br>
-                  ${book.publisher} <br>${book.first_publish_year}</p>
+                  <h4 class="card-title text-center fw-bold">${book.title}</h4>
+                  <p class="card-text">
+                  <ul style=" list-style-type: none;">
+                  <li><span class="fw-bold">Author Name: </span>${book.author_name}</li>
+                  <li><span class="fw-bold">Publisher Name: </span>${book.publisher}</li>
+                  <li><span class="fw-bold">First Published: </span>${book.first_publish_year}</li>
+                  </ul>
+                  </p>
                 </div>
-                <button>see more</button>
+                <button class="bg-info text-light fw-bold">See More About This</button>
               </div>
             `;
             findBook.appendChild(div);
         });
     }
-    
 }
-{/* <img src="" class="card-img-top" alt="..."></img> */}
